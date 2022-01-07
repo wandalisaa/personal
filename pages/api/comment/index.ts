7a095eab32c0be1832181d5770e58633197135cb
Wanda-Khalishah <wndkhlish@gmail.com>
2022-01-07 20:23:24 +0700

    debug

--- pages/api/comment/index.ts
+++ pages/api/comment/index.ts
@@ -2,20 +2,18 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { comments } from "../../../data/comment";
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
     res.status(200).json(comments);
   } else if (req.method === "POST") {
     const { comment } = req.body;
     const { nama } = req.body;
+    const date = Date.now();
     const newComment = {
-      id: toString(Date.now()),
+      id: toString(date),
       name: nama,
       text: comment,
-      time: toString(Date.now()),
+      time: toString(date),
     };
     comments.push(newComment);
     res.status(201).json(newComment);
